99ca02240c1da00c9a91b4ab48f39889891e95fb
aden <adou@calpoly.edu>
2026-04-03 12:32:00 -0700

    IE1: Step 8 Complete

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,15 +1,13 @@
 // src/MyApp.jsx
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
+
 
 function MyApp() {
     
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor",
-    }, // the rest of the data
-  ]);
+  const [characters, setCharacters] = useState([]);
+
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
       return i !== index;
@@ -19,6 +17,7 @@ function MyApp() {
   return (
   <div className="container">
     <Table characterData={characters} removeCharacter={removeOneCharacter} />
+    <Form />
   </div>
 );
 
