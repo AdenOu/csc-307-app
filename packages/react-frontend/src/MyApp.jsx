60cd1be6181298283ba5bc0cf5027c61e033b257
aden <adou@calpoly.edu>
2026-04-20 12:41:28 -0700

    IE3 Step 1 Done

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,12 +1,21 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
+
 
 
 function MyApp() {
     
   const [characters, setCharacters] = useState([]);
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+    });
+  }, []);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -14,9 +23,14 @@ function MyApp() {
     });
     setCharacters(updated);
   }
+  function fetchUsers() {
+    const promise = fetch("http://localhost:8000/users");
+    return promise;
+  }
+
   function updateList(person) {
     setCharacters([...characters, person]);
-}
+  }
   return (
   <div className="container">
     <Table characterData={characters} removeCharacter={removeOneCharacter} />
